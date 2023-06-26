93b33207b6c0402fa2861aea9e5daab33f849b56
loitrem <ski_fix@hotmail.com>
2023-06-26 12:57:11 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,155 @@
+//player character class
+class character {
+
+    //constructor
+    constructor(){
+
+    }
+
+    //upgrade character
+    upgrade() {
+        
+    }
+
+    //getter
+    getCharacter() {
+    
+        let newCharacter = {
+
+    
+        }
+}
+
+    //setter
+    setCharacter(){
+
+    }
+
+}
+
+//player character array class
+class characterArray {
+    
+    //iniate array
+    newCharacterArray (numOfEnemies) {
+    
+        let newCharacterArray = [];
+
+    }
+
+    //return array
+
+}
+
+//enemy class
+class enemy {
+
+    //constructor
+    constructor(){
+
+    }
+
+    //getter
+    getEnemy() {
+    
+        let newEnemy = {
+
+    
+        }
+}
+
+    //setter
+    setEnemy(){
+
+    }
+}
+
+//enemy array class
+class enemyArray {
+
+    //iniate array
+    newEnemyArray (numOfEnemies) {
+    
+        let newEnemyArray = [];
+
+    }
+    //return array
+
+}
+
+//animation class
+class animation {
+
+    //animate
+    animate () {
+
+    }
+
+    //attack right
+    attackRight () {
+
+    }
+
+    //attack left
+    attackLeft () {
+
+    }
+
+}
+
+//game logic class
+class gameLogic {
+
+    //is alive
+    isAlive(character) {
+
+    }
+
+    //does attack hit
+    doesItHit (attacker, defender) {
+
+    }
+
+    //character attack
+    attack (attacker, defender) {
+
+    }
+
+    //start game
+    startGame (){
+
+    }
+
+    //reset game
+    resetGame () {
+
+    }
+
+    //next round
+    nextRound () {
+
+    }
+
+    //display world
+    displayWorld (name) {
+
+    }
+
+    //fight - per character
+    oneCharacterAttack () {
+
+    }
+
+    //are there enemies left after each attack
+    enemiesLeft() {
+
+    }
+
+    //retreat
+    retreat() {
+
+    }
+
+}
+
+
