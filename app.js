84fedbeedab21177a32fe6541fdc362d7f0e12cf
loitrem <ski_fix@hotmail.com>
2023-06-26 17:31:57 -0400

    enemy array working and main party stats created

--- app.js
+++ app.js
@@ -2,8 +2,13 @@
 class character {
 
     //constructor
-    constructor(){
-
+    constructor(name, attack, defense, hp, mp, xp){
+        this.name = name;
+        this.attack = attack;
+        this.defense = defense;
+        this.hp = hp;
+        this.mp = mp;
+        this.xp = xp;
     }
 
     //upgrade character
@@ -14,67 +19,298 @@ class character {
     //getter
     getCharacter() {
     
+        //create object
         let newCharacter = {
-
-    
+            name: this.name,
+            attack: this.attack,
+            defense: this.defense,
+            hp: this.hp,
+            mp: this.mp,
+            xp: this.xp
         }
+
+        //return the object
+        return newCharacter;
 }
 
     //setter
-    setCharacter(){
+    setCharacter(updatedCharacter){
 
+        // create new character object
+        let tempCharacter = new character();
+
+        //set updated values into tempCharacter
+        tempCharacter.name = updatedCharacter.name;
+        tempCharacter.attack = updatedCharacter.attack;
+        tempCharacter.defense = updatedCharacter.defense;
+        tempCharacter.hp = updatedCharacter.hp;
+        tempCharacter.mp = updatedCharacter.mp;
+        tempCharacter.xp = updatedCharacter.xp;
+
+        //return the updated object
+        return tempCharacter;
     }
-
-}
-
-//player character array class
-class characterArray {
-    
-    //iniate array
-    newCharacterArray (numOfEnemies) {
-    
-        let newCharacterArray = [];
-
-    }
-
-    //return array
-
 }
 
 //enemy class
 class enemy {
 
     //constructor
-    constructor(){
-
+    constructor(type, attack,attackMax, attackMin, defense, defenseMax, defenseMin, hp, hpMax, hpMin, mp, mpMax, mpMin, xp, xpMax, xpMin){
+        this.type = type;
+        this.attack = attack;
+        this.attackMax = attackMax;
+        this.attackMin = attackMin;
+        this.defense = defense;
+        this.defenseMax = defenseMax;
+        this.defenseMin = defenseMin;
+        this.hp = hp;
+        this.hpMax = hpMax;
+        this.hpMin = hpMin;
+        this.mp = mp;
+        this.mpMax = mpMax;
+        this.mpMin = mpMin;
+        this.xp = xp;
+        this.xpMax = xpMax;
+        this.xpMin = xpMin;
     }
 
     //getter
     getEnemy() {
     
+        //create new object
         let newEnemy = {
-
-    
+            type: this.type,
+            attack: this.attack,
+            attackMax: this.attackMax,
+            attackMin: this.attackMin,
+            defense: this.defense,
+            defenseMax: this.defenseMax,
+            defenseMin: this.defenseMin,
+            hp: this.hp,
+            hpMax: this.hpMax,
+            hpMin: this.hpMin,
+            mp: this.mp,
+            mpMax: this.mpMax,
+            mpMin: this.mpMin,
+            xp: this.xp,
+            xpMax: this.xpMax,
+            xpMin: this.xpMin
         }
-}
 
-    //setter
-    setEnemy(){
-
+        //return the object
+        return newEnemy;
     }
 }
 
 //enemy array class
 class enemyArray {
 
+    //determind which enemy to add to the array
+    addEnemy (num) {
+
+        let monsterName = "";
+
+        if (num>=1&&num<=15){
+            monsterName = "blackBat";
+        } else if (num>=16&&num<=30){
+            monsterName = "blackSpider";
+        } else if (num>=31&&num<=42){
+            monsterName = "grayBat";
+        } else if (num>=43&&num<=54){
+            monsterName = "graySpider";
+        } else if (num>=55&&num<=66){
+            monsterName = "skeleton";
+        } else if (num>=67&&num<=76){
+            monsterName = "zombie";
+        } else if (num>=77&&num<=86){
+            monsterName = "whiteSpider";
+        } else if (num>=87&&num<=92){
+            monsterName = "blueSpider";
+        } else if (num>=93&&num<=97){
+            monsterName = "greenSpider";
+        } else if (num>=98&&num<=100){
+            monsterName = "demonBat";
+        }
+
+        //return monsterName
+        return monsterName;
+    }
+
+    //set ranges for monsters
+    monsterRange(name){
+
+        //created new enemy
+        let tempEnemy = new enemy();
+
+        //set max and min for enemy for random number generation
+        if (name==="blackBat"){
+            tempEnemy.type = name;
+            tempEnemy.attackMax = 4;
+            tempEnemy.attackMin = 1;
+            tempEnemy.defenseMax = 2;
+            tempEnemy.defenseMin = 1;
+            tempEnemy.hpMax = 8;
+            tempEnemy.hpMin = 5;
+            tempEnemy.mpMax = 0;
+            tempEnemy.mpMin = 0;
+            tempEnemy.xpMax = 11;
+            tempEnemy.xpMin = 2;
+        } else if (name==="blackSpider"){
+            tempEnemy.type = name;
+            tempEnemy.attackMax = 4;
+            tempEnemy.attackMin = 1;
+            tempEnemy.defenseMax = 2;
+            tempEnemy.defenseMin = 1;
+            tempEnemy.hpMax = 8;
+            tempEnemy.hpMin = 5;
+            tempEnemy.mpMax = 0;
+            tempEnemy.mpMin = 0;
+            tempEnemy.xpMax = 11;
+            tempEnemy.xpMin = 2;
+        } else if (name==="grayBat"){
+            tempEnemy.type = name;
+            tempEnemy.attackMax = 5;
+            tempEnemy.attackMin = 2;
+            tempEnemy.defenseMax = 3;
+            tempEnemy.defenseMin = 2;
+            tempEnemy.hpMax = 8;
+            tempEnemy.hpMin = 5;
+            tempEnemy.mpMax = 0;
+            tempEnemy.mpMin = 0;
+            tempEnemy.xpMax = 12;
+            tempEnemy.xpMin = 4;
+        } else if (name==="graySpider"){
+            tempEnemy.type = name;
+            tempEnemy.attackMax = 5;
+            tempEnemy.attackMin = 2;
+            tempEnemy.defenseMax = 3;
+            tempEnemy.defenseMin = 2;
+            tempEnemy.hpMax = 8;
+            tempEnemy.hpMin = 5;
+            tempEnemy.mpMax = 0;
+            tempEnemy.mpMin = 0;
+            tempEnemy.xpMax = 12;
+            tempEnemy.xpMin = 5;
+        } else if (name==="skeleton"){
+            tempEnemy.type = name;
+            tempEnemy.attackMax = 5;
+            tempEnemy.attackMin = 2;
+            tempEnemy.defenseMax = 2;
+            tempEnemy.defenseMin = 1;
+            tempEnemy.hpMax = 10;
+            tempEnemy.hpMin = 7;
+            tempEnemy.mpMax = 0;
+            tempEnemy.mpMin = 0;
+            tempEnemy.xpMax = 14;
+            tempEnemy.xpMin = 5;
+        } else if (name==="zombie"){
+            tempEnemy.type = name;
+            tempEnemy.attackMax = 5;
+            tempEnemy.attackMin = 2;
+            tempEnemy.defenseMax = 2;
+            tempEnemy.defenseMin = 1;
+            tempEnemy.hpMax = 10;
+            tempEnemy.hpMin = 7;
+            tempEnemy.mpMax = 0;
+            tempEnemy.mpMin = 0;
+            tempEnemy.xpMax = 14;
+            tempEnemy.xpMin = 6;
+        } else if (name==="whiteSpider"){
+            tempEnemy.type = name;
+            tempEnemy.attackMax = 7;
+            tempEnemy.attackMin = 4;
+            tempEnemy.defenseMax = 5;
+            tempEnemy.defenseMin = 3;
+            tempEnemy.hpMax = 10;
+            tempEnemy.hpMin = 7;
+            tempEnemy.mpMax = 2;
+            tempEnemy.mpMin = 1;
+            tempEnemy.xpMax = 15;
+            tempEnemy.xpMin = 8;
+        } else if (name==="blueSpider"){
+            tempEnemy.type = name;
+            tempEnemy.attackMax = 8;
+            tempEnemy.attackMin = 5;
+            tempEnemy.defenseMax = 5;
+            tempEnemy.defenseMin = 3;
+            tempEnemy.hpMax = 12;
+            tempEnemy.hpMin = 7;
+            tempEnemy.mpMax = 3;
+            tempEnemy.mpMin = 1;
+            tempEnemy.xpMax = 18;
+            tempEnemy.xpMin = 12;
+        } else if (name==="greenSpider"){
+            tempEnemy.type = name;
+            tempEnemy.attackMax = 11;
+            tempEnemy.attackMin = 6;
+            tempEnemy.defenseMax = 6;
+            tempEnemy.defenseMin = 4;
+            tempEnemy.hpMax = 8;
+            tempEnemy.hpMin = 5;
+            tempEnemy.mpMax = 0;
+            tempEnemy.mpMin = 0;
+            tempEnemy.xpMax = 22;
+            tempEnemy.xpMin = 14;
+        } else if (name==="demonBat"){
+            tempEnemy.type = name;
+            tempEnemy.attackMax = 15;
+            tempEnemy.attackMin = 10;
+            tempEnemy.defenseMax = 7;
+            tempEnemy.defenseMin = 5;
+            tempEnemy.hpMax = 30;
+            tempEnemy.hpMin = 20;
+            tempEnemy.mpMax = 4;
+            tempEnemy.mpMin = 1;
+            tempEnemy.xpMax = 40;
+            tempEnemy.xpMin = 25;
+        }
+
+        //return range array
+        return tempEnemy;
+
+    }
+
     //iniate array
     newEnemyArray (numOfEnemies) {
     
+        //create empty array
         let newEnemyArray = [];
+        let newRangeArray = [];
 
+        //create a new enemy
+        let newTempEnemy = new enemy();
+
+        //loop for each enemy that will be added
+        for (let i = 0; i < numOfEnemies; i++){
+
+            // random for hull
+            let enemytypeNum = Math.floor(Math.floor(Math.random()*(101-1)+1));
+            //adds to new range array by called add enemy and using that returned name to call monster range and return an array
+            newTempEnemy = this.monsterRange(this.addEnemy(enemytypeNum));
+
+            // random for attack
+            newTempEnemy.attack = Math.floor(Math.floor(Math.random()*(newTempEnemy.attackMax-newTempEnemy.attackMin)+newTempEnemy.attackMin));
+
+            // random for defense
+            newTempEnemy.defense = Math.floor(Math.floor(Math.random()*(newTempEnemy.defenseMax-newTempEnemy.defenseMin)+newTempEnemy.defenseMin));
+
+            // random for hp
+            newTempEnemy.hp = Math.floor(Math.floor(Math.random()*(newTempEnemy.hpMax-newTempEnemy.hpMin)+newTempEnemy.hpMin));
+
+            // random for mp
+            newTempEnemy.mp = Math.floor(Math.floor(Math.random()*(newTempEnemy.mpMax-newTempEnemy.mpMin)+newTempEnemy.mpMin));
+
+            // random for xp
+            newTempEnemy.xp = Math.floor(Math.floor(Math.random()*(newTempEnemy.xpMax-newTempEnemy.xpMin)+newTempEnemy.xpMin));
+
+            //add to array
+            newEnemyArray.push(newTempEnemy);
+        }
+        //return array
+        return newEnemyArray;
+        
     }
-    //return array
-
 }
 
 //animation class
@@ -157,4 +393,10 @@ class gameLogic {
 
 }
 
+const kazuma = new character("kazuma", 10, 5, 20, 1, 0);
+const darkness = new character("darkness", 2, 9, 75, 0, 0);
+const megumin = new character("megumin", 100, 2, 10, 1, 0);
+const aqua = new character("aqua", 15, 4, 20, 10, 0);
 
+let test = new enemyArray;
+console.log(test.newEnemyArray(4));
